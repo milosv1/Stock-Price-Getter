18fc09e4de1a66a727879b21227ee8a4bcbf9ea6
Milos <milosvuksanovic53@gmail.com>
2020-12-09 16:44:58 +1100

    Added first comment.

---
+++ s_p_g.py
@@ -0,0 +1,3 @@
+#s_p_g.py 
+#This script allows the user to get stock prices from companies within a specific industry.
+#As an example, when you use the command "--tech tech" you will be getting pricing of stocks from that industry i.e Google, Apple etc.
